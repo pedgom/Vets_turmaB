74fad5b528c07d6eab789ab2f9681d75d476ff4c
Pedro Gomes <aluno21279@ipt.pt>
2020-03-31 15:34:04 +0100

    criação das classes que representam as minhas tabelas

---
+++ Vets/Vets/Models/Donos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vets.Models
+{
+    public class Donos
+    {
+        public int ID { get; set; }
+
+        public string Nomes { get; set; }
+
+        public int NIF { get; set; }
+    }
+}
